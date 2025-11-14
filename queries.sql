f5bcf14b462726b35e589173d574e24b727431df
VikitoriaR <viktoria.reutova@hotmail.com>
2025-11-14 19:28:37 +0200

    Project Files

--- queries.sql
+++ queries.sql
@@ -68,3 +68,52 @@ inner join employees empl on sales.sales_person_id = empl.employee_id
 inner join products prod on sales.product_id = prod.product_id
 group by seller, day_of_week, extract (isodow from sales.sale_date)
 order by extract (isodow from sales.sale_date), seller;
+
+
+
+--6.1 Запрос на количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+. Итоговая таблица должна быть отсортирована по возрастным группам.
+with tab as (select *,
+case when age between 16 and 25 then '16-25'
+     when age between 26 and 40 then '26-40'
+     when age >40 then '40+'
+end as age_category
+from customers)
+
+select age_category, count(age_category)
+from tab
+group by age_category
+order by age_category
+
+
+--6.2 Во втором отчете предоставьте данные по количеству уникальных покупателей и выручке, которую они принесли.
+--Сгруппируйте данные по дате, которая представлена в числовом виде ГОД-МЕСЯЦ. 
+select selling_month, count(distinct customer_id) as total_customers,sum(income) as income
+from (select sales.*,
+      to_char(date_trunc('month', sales.sale_date), 'yyyy-mm') as selling_month,
+      sales.quantity* prod.price as income
+      from sales
+      inner join products prod on sales.product_id = prod.product_id)
+group by selling_month
+order by selling_month asc
+
+
+--6.3 Третий отчет следует составить о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0).
+--Итоговая таблица должна быть отсортирована по id покупателя.
+with tab as(     
+            select sales.*,
+            to_char(date_trunc('month', sales.sale_date), 'yyyy-mm') as selling_month,
+            min(sale_date) over (partition by sales.customer_id) as minsaledate,
+            sales.quantity* prod.price as income,
+            concat(cust.first_name, ' ', cust.last_name) as custname,
+            concat(empl.first_name, ' ', empl.last_name) as salespname
+            from sales
+            inner join products prod on sales.product_id = prod.product_id
+            inner join customers cust on sales.customer_id = cust.customer_id
+            inner join employees empl on sales.sales_person_id = empl.employee_id) 
+
+select custname as customer, sale_date, salespname as seller
+from tab
+where sale_date = minsaledate
+and income = 0 
+group by custname, sale_date, salespname, customer_id
+order by customer_id asc
