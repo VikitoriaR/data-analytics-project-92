01cfcc31154b3c50c17714a3a159fe6d46ba4f1c
VikitoriaR <viktoria.reutova@hotmail.com>
2025-11-14 17:29:17 +0200

    Project files

--- queries.sql
+++ queries.sql
@@ -23,3 +23,48 @@ limit 10
 --4 Считаем общее количество покупателей из таблицы customers.
 select count(distinct customer_id) as customers_count
 from customers
+
+
+--5.1 Запрос на извлечение данных о продавце, суммарной выручке с проданных товаров и количестве проведенных сделок, и отсортирована по убыванию выручки
+select concat (empl.first_name, ' ', empl.last_name) as seller, count(sales.sales_person_id) as operations
+       ,sum(sales.quantity * prod.price) as income
+from sales
+inner join employees empl on sales.sales_person_id = empl.employee_id 
+inner join products prod on sales.product_id = prod.product_id 
+group by seller
+order by income desc
+limit 10
+
+
+--5.2 Второй отчет содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам. Таблица отсортирована по выручке по возрастанию.
+with tab as (
+   select concat (empl.first_name, ' ', empl.last_name) as seller
+          ,round(avg(sales.quantity * prod.price)) avg_sales
+          ,sum(sales.quantity * prod.price) as income
+    from sales
+    inner join employees empl on sales.sales_person_id = empl.employee_id 
+    inner join products prod on sales.product_id = prod.product_id 
+    group by seller
+    order by avg_sales)
+
+select seller, avg_sales as average_income
+from tab
+where avg_sales < (select round(avg(sales.quantity * prod.price))
+                        from sales
+                        inner join employees empl on sales.sales_person_id = empl.employee_id 
+                        inner join products prod on sales.product_id = prod.product_id)
+order by average_income asc;
+
+
+
+--5.3 Третий отчет содержит информацию о выручке по дням недели.
+--Каждая запись содержит имя и фамилию продавца, день недели и суммарную выручку.
+--Отсортируйте данные по порядковому номеру дня недели и seller
+select concat (empl.first_name, ' ', empl.last_name) as seller
+       ,trim(to_char (sales.sale_date, 'Day')) as day_of_week 
+       ,round(sum(sales.quantity * prod.price)) as total
+from sales
+inner join employees empl on sales.sales_person_id = empl.employee_id 
+inner join products prod on sales.product_id = prod.product_id
+group by seller, day_of_week, extract (isodow from sales.sale_date)
+order by extract (isodow from sales.sale_date), seller;
