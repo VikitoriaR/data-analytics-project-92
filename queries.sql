6a049e4012de677cb406c9d6a3684da819d52261
VikitoriaR <viktoria.reutova@hotmail.com>
2025-11-15 11:07:00 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,102 +1,120 @@
--- Считаем общее количество покупателей из таблицы customers.
-SELECT
-    COUNT(DISTINCT customer_id) AS customers_count
-FROM customers;
+--начало
+-- скрипт считает общее число записей в таблице customers
 
--- Продавец, сделки и прибыль
-SELECT
-    CONCAT(empl.first_name, ' ', empl.last_name) AS seller,
-    COUNT(sales.sales_person_id) AS operations,
-    FLOOR(SUM(sales.quantity * prod.price)) AS income
-FROM sales
-    INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
-    INNER JOIN products AS prod ON sales.product_id = prod.product_id
-GROUP BY seller
-ORDER BY income DESC
-LIMIT 10;
+select count(*) as customers_count
 
--- Продавцы, чья средняя выручка за сделку меньше средней общей
-WITH tab AS (
-    SELECT
-        CONCAT(empl.first_name, ' ', empl.last_name) AS seller,
-        FLOOR(AVG(sales.quantity * prod.price)) AS avg_sales,
-        SUM(sales.quantity * prod.price) AS income
-    FROM sales
-        INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
-        INNER JOIN products AS prod ON sales.product_id = prod.product_id
-    GROUP BY seller
-)
-SELECT
-    seller,
-    avg_sales AS average_income
-FROM tab
-WHERE avg_sales < (
-    SELECT
-        FLOOR(AVG(sales.quantity * prod.price))
-    FROM sales
-        INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
-        INNER JOIN products AS prod ON sales.product_id = prod.product_id
-)
-ORDER BY average_income ASC;
+from customers;
 
--- Выручки по дням недели
-SELECT
-    CONCAT(empl.first_name, ' ', empl.last_name) AS seller,
-    TRIM(TO_CHAR(sales.sale_date, 'day')) AS day_of_week,
-    FLOOR(SUM(sales.quantity * prod.price)) AS income
-FROM sales
-    INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
-    INNER JOIN products AS prod ON sales.product_id = prod.product_id
-GROUP BY
-    seller,
-    day_of_week,
-    EXTRACT(ISODOW FROM sales.sale_date)
-ORDER BY
-    EXTRACT(ISODOW FROM sales.sale_date),
-    seller;
-    
--- Покупатели по возрастным группам
-WITH tab AS (
-    SELECT
-        *,
-        CASE
-            WHEN age BETWEEN 16 AND 25 THEN '16-25'
-            WHEN age BETWEEN 26 AND 40 THEN '26-40'
-            WHEN age > 40 THEN '40+'
-        END AS age_category
-    FROM customers
-)
-SELECT
-    age_category,
-    COUNT(age_category) AS age_count
-FROM tab
-GROUP BY age_category
-ORDER BY age_category;
 
--- Покупатели, первая покупка которых пришлась на время проведения акции
-WITH tab AS (
+-- Подготовьте отчет с продавцами у которых наибольшая выручка
+
+select
+	employees.first_name || ' ' || employees.last_name as seller,
+	COUNT(sales.sales_id) as operations,
+	FLOOR(SUM(products.price * sales.quantity)) as income
+from sales
+inner join employees on
+	sales.sales_person_id = employee_id
+inner join products on
+	sales.product_id = products.product_id
+group by employees.first_name, employees.last_name
+order by income desc
+limit 10;
+
+
+-- Второй отчет содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам. Таблица отсортирована по выручке по возрастанию.
+
+select
+    employees.first_name || ' ' || employees.last_name AS seller,
+    FLOOR(SUM(sales.quantity * products.price) / COUNT(sales.sales_id)) AS average_income
+from employees
+inner join sales on employees.employee_id = sales.sales_person_id
+inner join products on sales.product_id = products.product_id
+group by employees.first_name, employees.last_name
+HAVING (SUM(sales.quantity * products.price) / COUNT(sales.sales_id)) <
+       (
+         SELECT SUM(sales.quantity * products.price) / COUNT(sales.sales_id)
+         FROM sales
+         INNER JOIN products ON sales.product_id = products.product_id
+       )
+order by average_income ASC;
+
+
+-- Третий отчет содержит информацию о выручке по дням недели. Каждая запись содержит имя и фамилию продавца, день недели и суммарную выручку. Отсортируйте данные по порядковому номеру дня недели и seller
+
+select
+	employees.first_name || ' ' || employees.last_name AS seller,
+	TO_CHAR(sales.sale_date, 'Day') AS day_of_week,
+	FLOOR(SUM(products.price * sales.quantity)) as income
+from sales
+inner join employees on employees.employee_id = sales.sales_person_id
+inner join products on sales.product_id = products.product_id
+group by employees.first_name, employees.last_name, TO_CHAR(sales.sale_date, 'Day'), EXTRACT(DOW FROM sales.sale_date)
+order by EXTRACT(DOW FROM sales.sale_date), seller asc;
+
+-- отчет - количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+. Итоговая таблица должна быть отсортирована по возрастным группам и содержать следующие поля:
+select
+  age_category,
+  age_count
+from (
+  SELECT
+    CASE
+      WHEN customers.age BETWEEN 16 AND 25 THEN '16-25'
+      WHEN customers.age BETWEEN 26 AND 40 THEN '26-40'
+      WHEN customers.age > 40 THEN '40+'
+      ELSE 'unknown'
+    END AS age_category,
+    COUNT(customers.customer_id) AS age_count
+  from customers
+  group by
+    CASE
+      WHEN customers.age BETWEEN 16 AND 25 THEN '16-25'
+      WHEN customers.age BETWEEN 26 AND 40 THEN '26-40'
+      WHEN customers.age > 40 THEN '40+'
+      ELSE 'unknown'
+    end
+) as categories
+order by
+  CASE age_category
+    WHEN '16-25' THEN 1
+    WHEN '26-40' THEN 2
+    WHEN '40+' THEN 3
+    ELSE 4
+  end;
+
+
+--Во втором отчете предоставьте данные по количеству уникальных покупателей и выручке, которую они принесли.
+--Сгруппируйте данные по дате, которая представлена в числовом виде ГОД-МЕСЯЦ.
+--Итоговая таблица должна быть отсортирована по дате по возрастанию и содержать следующие поля:
+select
+	EXTRACT(YEAR FROM sales.sale_date) * 100 + EXTRACT(MONTH FROM sales.sale_date) AS date,
+	count(distinct customers.customer_id) as total_customers,
+	FLOOR(sum(products.price * sales.quantity)) as income
+from sales
+inner join customers on customers.customer_id = sales.customer_id
+inner join products on sales.product_id = products.product_id
+group by EXTRACT(YEAR FROM sales.sale_date) * 100 + EXTRACT(MONTH FROM sales.sale_date)
+order by date asc;
+
+
+--Третий отчет следует составить о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0).
+--Итоговая таблица должна быть отсортирована по id покупателя. Таблица состоит из следующих полей:
+SELECT
+    customers.first_name || ' ' || customers.last_name AS customer,
+    sales.sale_date,
+    employees.first_name || ' ' || employees.last_name AS seller
+FROM (
     SELECT
         sales.*,
-        TO_CHAR(DATE_TRUNC('month', sales.sale_date), 'yyyy-mm') AS selling_month,
-        MIN(sale_date) OVER (PARTITION BY sales.customer_id) AS minsaledate,
-        sales.quantity * prod.price AS income,
-        CONCAT(cust.first_name, ' ', cust.last_name) AS custname,
-        CONCAT(empl.first_name, ' ', empl.last_name) AS salespname
+        ROW_NUMBER() OVER (
+            PARTITION BY sales.customer_id
+            ORDER BY sales.sale_date
+        ) AS rn
     FROM sales
-        INNER JOIN products AS prod ON sales.product_id = prod.product_id
-        INNER JOIN customers AS cust ON sales.customer_id = cust.customer_id
-        INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
-)
-SELECT
-    custname AS customer,
-    sale_date,
-    salespname AS seller
-FROM tab
-WHERE sale_date = minsaledate
-    AND income = 0
-GROUP BY
-    custname,
-    sale_date,
-    salespname,
-    customer_id
-ORDER BY customer_id ASC;
+    INNER JOIN products ON products.product_id = sales.product_id
+    WHERE products.price = 0
+) AS sales
+INNER JOIN customers ON customers.customer_id = sales.customer_id
+INNER JOIN employees ON employees.employee_id = sales.sales_person_id
+WHERE rn = 1
+order by customers.customer_id;
