22009bcc8bde62e80c5fdadd20ae0b939ad22b48
VikitoriaR <viktoria.reutova@hotmail.com>
2025-11-15 11:02:29 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,137 +1,102 @@
 -- Считаем общее количество покупателей из таблицы customers.
-select
-    count(distinct customer_id) as customers_count
-from customers;
+SELECT
+    COUNT(DISTINCT customer_id) AS customers_count
+FROM customers;
 
--- Продавец, сделки и прибыль.
-select
-    concat(empl.first_name, ' ', empl.last_name) as seller,
-    count(sales.sales_person_id) as operations,
-    floor(sum(sales.quantity * prod.price)) as income
-from sales
-inner join employees as empl
-    on sales.sales_person_id = empl.employee_id
-inner join products as prod
-    on sales.product_id = prod.product_id
-group by seller
-order by income desc
-limit 10;
+-- Продавец, сделки и прибыль
+SELECT
+    CONCAT(empl.first_name, ' ', empl.last_name) AS seller,
+    COUNT(sales.sales_person_id) AS operations,
+    FLOOR(SUM(sales.quantity * prod.price)) AS income
+FROM sales
+    INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
+    INNER JOIN products AS prod ON sales.product_id = prod.product_id
+GROUP BY seller
+ORDER BY income DESC
+LIMIT 10;
 
--- Продавцы, чья средняя выручка за сделку меньше средней по всем.
-with tab as (
-    select
-        concat(empl.first_name, ' ', empl.last_name) as seller,
-        floor(avg(sales.quantity * prod.price)) as avg_sales,
-        sum(sales.quantity * prod.price) as income
-    from sales
-    inner join employees as empl
-        on sales.sales_person_id = empl.employee_id
-    inner join products as prod
-        on sales.product_id = prod.product_id
-    group by seller
+-- Продавцы, чья средняя выручка за сделку меньше средней общей
+WITH tab AS (
+    SELECT
+        CONCAT(empl.first_name, ' ', empl.last_name) AS seller,
+        FLOOR(AVG(sales.quantity * prod.price)) AS avg_sales,
+        SUM(sales.quantity * prod.price) AS income
+    FROM sales
+        INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
+        INNER JOIN products AS prod ON sales.product_id = prod.product_id
+    GROUP BY seller
 )
-select
+SELECT
     seller,
-    avg_sales as average_income
-from tab
-where avg_sales < (
-    select
-        floor(avg(sales.quantity * prod.price))
-    from sales
-    inner join employees as empl
-        on sales.sales_person_id = empl.employee_id
-    inner join products as prod
-        on sales.product_id = prod.product_id
+    avg_sales AS average_income
+FROM tab
+WHERE avg_sales < (
+    SELECT
+        FLOOR(AVG(sales.quantity * prod.price))
+    FROM sales
+        INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
+        INNER JOIN products AS prod ON sales.product_id = prod.product_id
 )
-order by average_income asc;
+ORDER BY average_income ASC;
 
--- Выручки по дням недели.
-select
-    concat(empl.first_name, ' ', empl.last_name) as seller,
-    trim(to_char(sales.sale_date, 'day')) as day_of_week,
-    floor(sum(sales.quantity * prod.price)) as income
-from sales
-inner join employees as empl
-    on sales.sales_person_id = empl.employee_id
-inner join products as prod
-    on sales.product_id = prod.product_id
-group by
+-- Выручки по дням недели
+SELECT
+    CONCAT(empl.first_name, ' ', empl.last_name) AS seller,
+    TRIM(TO_CHAR(sales.sale_date, 'day')) AS day_of_week,
+    FLOOR(SUM(sales.quantity * prod.price)) AS income
+FROM sales
+    INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
+    INNER JOIN products AS prod ON sales.product_id = prod.product_id
+GROUP BY
     seller,
     day_of_week,
-    extract(isodow from sales.sale_date)
-order by
-    extract(isodow from sales.sale_date),
+    EXTRACT(ISODOW FROM sales.sale_date)
+ORDER BY
+    EXTRACT(ISODOW FROM sales.sale_date),
     seller;
-
--- Покупатели по возрастным группам.
-with tab as (
-    select
+    
+-- Покупатели по возрастным группам
+WITH tab AS (
+    SELECT
         *,
-        case
-            when age between 16 and 25 then '16-25'
-            when age between 26 and 40 then '26-40'
-            when age > 40 then '40+'
-        end as age_category
-    from customers
+        CASE
+            WHEN age BETWEEN 16 AND 25 THEN '16-25'
+            WHEN age BETWEEN 26 AND 40 THEN '26-40'
+            WHEN age > 40 THEN '40+'
+        END AS age_category
+    FROM customers
 )
-select
+SELECT
     age_category,
-    count(age_category) as age_count
-from tab
-group by age_category
-order by age_category;
+    COUNT(age_category) AS age_count
+FROM tab
+GROUP BY age_category
+ORDER BY age_category;
 
--- Количество уникальных покупателей по возрастным группам.
-with tab as (
-    select
-        *,
-        case
-            when age between 16 and 25 then '16-25'
-            when age between 26 and 40 then '26-40'
-            when age > 40 then '40+'
-        end as age_category
-    from customers
-)
-select
-    age_category,
-    count(distinct customer_id) as unique_customers
-from tab
-group by age_category
-order by age_category;
-
--- Покупатели, первая покупка которых пришлась на время проведения
-with tab as (
-    select
+-- Покупатели, первая покупка которых пришлась на время проведения акции
+WITH tab AS (
+    SELECT
         sales.*,
-        to_char(
-            date_trunc('month', sales.sale_date),
-            'yyyy-mm'
-        ) as selling_month,
-        min(sale_date) over (
-            partition by sales.customer_id
-        ) as minsaledate,
-        sales.quantity * prod.price as income,
-        concat(cust.first_name, ' ', cust.last_name) as custname,
-        concat(empl.first_name, ' ', empl.last_name) as salespname
-    from sales
-    inner join products as prod
-        on sales.product_id = prod.product_id
-    inner join customers as cust
-        on sales.customer_id = cust.customer_id
-    inner join employees as empl
-        on sales.sales_person_id = empl.employee_id
+        TO_CHAR(DATE_TRUNC('month', sales.sale_date), 'yyyy-mm') AS selling_month,
+        MIN(sale_date) OVER (PARTITION BY sales.customer_id) AS minsaledate,
+        sales.quantity * prod.price AS income,
+        CONCAT(cust.first_name, ' ', cust.last_name) AS custname,
+        CONCAT(empl.first_name, ' ', empl.last_name) AS salespname
+    FROM sales
+        INNER JOIN products AS prod ON sales.product_id = prod.product_id
+        INNER JOIN customers AS cust ON sales.customer_id = cust.customer_id
+        INNER JOIN employees AS empl ON sales.sales_person_id = empl.employee_id
 )
-select
-    custname as customer,
+SELECT
+    custname AS customer,
     sale_date,
-    salespname as seller
-from tab
-where
-    sale_date = minsaledate
-    and income = 0
-group by
+    salespname AS seller
+FROM tab
+WHERE sale_date = minsaledate
+    AND income = 0
+GROUP BY
     custname,
     sale_date,
     salespname,
     customer_id
-order by customer_id asc;
+ORDER BY customer_id ASC;
